64697ae71532f519d00a6de98c93e2d82ce83782
Muhammad Zaid <zaidkx37@gmail.com>
2025-11-02 05:13:17 -0800

    added secret_key and session

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from app.config import config
+
+from app.config import config, Config
 from app.database import Base, engine
 
 def create_app(config_name='default'):
@@ -9,6 +10,14 @@ def create_app(config_name='default'):
 
     # Load configuration
     app.config.from_object(config[config_name])
+    app.config.update(
+        SESSION_COOKIE_SECURE=True,
+        SESSION_COOKIE_SAMESITE='Lax',
+        SESSION_COOKIE_HTTPONLY=True
+        )
+    
+    # load SECRET_KEY from environment variable
+    app.config['SECRET_KEY'] = Config.SECRET_KEY
 
     # Import models to ensure they're registered with Base
     from app.models import User, Quiz, Result, Settings
